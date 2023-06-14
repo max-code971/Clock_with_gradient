0c8a7d01d135179b3459c38f5b82c74754297f70
Max ⚡ <97020506+max-code971@users.noreply.github.com>
2023-06-14 09:40:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+clock();
+
+function clock() {
+  const date = new Date();
+
+  const hours = ((date.getHours() + 11) % 12 + 1);
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  
+  const hour = hours * 30;
+  const minute = minutes * 6;
+  const second = seconds * 6;
+  
+  document.querySelector('.hour').style.transform = `rotate(${hour}deg)`
+  document.querySelector('.minute').style.transform = `rotate(${minute}deg)`
+  document.querySelector('.second').style.transform = `rotate(${second}deg)`
+}
+
+setInterval(clock, 1000);
